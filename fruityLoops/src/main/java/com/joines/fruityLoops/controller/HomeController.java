732845fcfe99e5a1711ca7096f14b57852ab77b8
Adam Joines <amountcastlej@gmail.com>
2023-09-12 23:07:13 -0400

    Add files via upload

---
+++ fruityLoops/src/main/java/com/joines/fruityLoops/controller/HomeController.java
@@ -0,0 +1,10 @@
+package com.joines.fruityLoops.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+}
+
+
---
+++ fruityLoops/src/test/java/com/joines/fruityLoops/FruityLoopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joines.fruityLoops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruityLoopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
